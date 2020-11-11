eee7a51836130dc093b46cb0f456d78c6ff2fe65
xwhuang@qnap <azraelxyz@users.noreply.github.com>
2020-11-11 15:32:29 +0000

    Update cloudclient.py

--- src/quiz/backend/cloudclient.py
+++ src/quiz/backend/cloudclient.py
@@ -18,26 +18,24 @@ class CloudClientAbstract(object):
     def upload_file(self, parent_id, name, data):
         ''' Upload a file with name and data under parent_id
 
-        e.g. 
-
         @param  parent_id: the parent id 
         @type   parent_id: str
         @param  name: the name of new file
         @type   name: str
-        @param  data: a file-like object of source
-        @type   data: bytes
-        @return the metadata of new node
+        @param  data: content of the file
+        @type   data: str
+        @return the metadata of the new node
         @rtype  Node
         '''
 
     def create_folder(self, parent_id, name):
-        '''Create a new folder with name under parent_id
+        ''' Create a new folder with name under parent_id
 
         @param  parent_id: the parent id
         @type   parent_id: str
         @param  name: the name of new file
         @type   name: str
-        @return the metadata of new node
+        @return the metadata of the new node
         @rtype  Node
         '''
 
@@ -45,7 +43,7 @@ class CloudClientAbstract(object):
         ''' List children under specific folder_id
 
         @param  folder_id: the folder id
-        @param  _id: unicode
+        @param  folder_id: str
         @return:    list of Node object
         @rtype:     list
         '''
